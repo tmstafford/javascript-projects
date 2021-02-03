6e051a1b5340ed4d75d480ee3dc67a925492c185
tmstafford <tatumstafford1@gmail.com>
2021-02-02 19:10:00 -0600

    project 4 navbar complete

--- 04-navbar/app.js
+++ 04-navbar/app.js
@@ -3,3 +3,13 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+// select DOM variables
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+function displayNav() {
+    links.classList.toggle('show-links');
+};
+
+navToggle.addEventListener('click', displayNav);
